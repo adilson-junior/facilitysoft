b67a2dfcbab6b593ade30963ce146b7aeaed8676
Adilson Junior <adilsonvajr@gmail.com>
2020-04-25 17:13:59 -0300

    Projeto criado

---
+++ src/main/java/br/com/facilitysoft/facilitysoft/FacilitysoftApplication.java
@@ -0,0 +1,13 @@
+package br.com.facilitysoft.facilitysoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitysoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitysoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/facilitysoft/facilitysoft/FacilitysoftApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.facilitysoft.facilitysoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitysoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
