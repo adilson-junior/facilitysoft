2f12b2e397dc02369d8d8133cd7ff29a3cbcc34d
adilson-junior <adilsonvajr@gmail.com>
2020-04-26 12:48:32 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/facilitysoft/facilitysoft/FacilitysoftApplication.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/FacilitysoftApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.facilitysoft.facilitysoft.dominio.Categoria;
+import br.com.facilitysoft.facilitysoft.dominio.Produto;
 import br.com.facilitysoft.facilitysoft.repositories.CategoriaRepository;
+import br.com.facilitysoft.facilitysoft.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class FacilitysoftApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilitysoftApplication.class, args);
@@ -23,10 +27,24 @@ public class FacilitysoftApplication implements CommandLineRunner{
 	//metodo auxiliar para criar os objetos no banco de teste
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		//add os produtos as categorias e vice versa
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
