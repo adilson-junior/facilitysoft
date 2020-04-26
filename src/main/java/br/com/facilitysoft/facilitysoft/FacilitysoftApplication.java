f48b9dda4d0139e70717f655a8eddd0ee992f059
adilson-junior <adilsonvajr@gmail.com>
2020-04-26 12:02:58 -0300

    Criando operação de instanciaçao dos objetos teste

--- src/main/java/br/com/facilitysoft/facilitysoft/FacilitysoftApplication.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/FacilitysoftApplication.java
@@ -1,13 +1,33 @@
 package br.com.facilitysoft.facilitysoft;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.facilitysoft.facilitysoft.dominio.Categoria;
+import br.com.facilitysoft.facilitysoft.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class FacilitysoftApplication {
+public class FacilitysoftApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilitysoftApplication.class, args);
+	}
+
+	//metodo auxiliar para criar os objetos no banco de teste
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
