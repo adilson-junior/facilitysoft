e4c2dfa7636af6bee2ccd677a275491d07985ab6
adilson-junior <adilsonvajr@gmail.com>
2020-04-28 10:05:46 -0300

    Deletendo uma Categoria com DELETE

--- src/main/java/br/com/facilitysoft/facilitysoft/services/CategoriaService.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.facilitysoft.facilitysoft.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.facilitysoft.facilitysoft.dominio.Categoria;
 import br.com.facilitysoft.facilitysoft.repositories.CategoriaRepository;
+import br.com.facilitysoft.facilitysoft.services.exceptions.DataIntegrityException;
 import br.com.facilitysoft.facilitysoft.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);	
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que contenha produtos.");
+		}
+			
 	}
 
 }
