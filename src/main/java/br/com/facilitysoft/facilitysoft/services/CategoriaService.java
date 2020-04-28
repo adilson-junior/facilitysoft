bbbb4626a260ce05b3d7195eb4cb740f6aa58ba0
adilson-junior <adilsonvajr@gmail.com>
2020-04-28 13:18:18 -0300

    Validação sintatica com Bean validation

--- src/main/java/br/com/facilitysoft/facilitysoft/services/CategoriaService.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.facilitysoft.facilitysoft.dominio.Categoria;
+import br.com.facilitysoft.facilitysoft.dto.CategoriaDTO;
 import br.com.facilitysoft.facilitysoft.repositories.CategoriaRepository;
 import br.com.facilitysoft.facilitysoft.services.exceptions.DataIntegrityException;
 import br.com.facilitysoft.facilitysoft.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {	
 		PageRequest pageRequest= PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	//metodo auxiliar para instanciar uma Categoria apartir de um DTO
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
