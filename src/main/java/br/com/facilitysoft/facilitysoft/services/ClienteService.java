58a066636220c691ea19da6ba4e1d8d61b160158
adilson-junior <adilsonvajr@gmail.com>
2020-05-01 10:15:59 -0300

    PUT, DELETE E GET para Cliente

--- src/main/java/br/com/facilitysoft/facilitysoft/services/ClienteService.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.facilitysoft.facilitysoft.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.facilitysoft.facilitysoft.dominio.Cliente;
+import br.com.facilitysoft.facilitysoft.dto.ClienteDTO;
 import br.com.facilitysoft.facilitysoft.repositories.ClienteRepository;
+import br.com.facilitysoft.facilitysoft.services.exceptions.DataIntegrityException;
 import br.com.facilitysoft.facilitysoft.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,6 +26,42 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);	
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Cliente com pedidos relacionados.");
+		}
+			
+	}
+
+	public List<Cliente> findAll() {		
+		return repo.findAll();
+	}
+	 
+	//metodo que faz a paginação da lista
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {	
+		PageRequest pageRequest= PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	//metodo auxiliar para instanciar uma Cliente apartir de um DTO
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 
 }
