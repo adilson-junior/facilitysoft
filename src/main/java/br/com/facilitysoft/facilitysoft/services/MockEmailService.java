1410fb3bdcb022f859c082d52260b6ef47f71609
adilson-junior <adilsonvajr@gmail.com>
2020-05-11 14:16:46 -0300

    Email com HTMl

--- src/main/java/br/com/facilitysoft/facilitysoft/services/MockEmailService.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package br.com.facilitysoft.facilitysoft.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,6 +15,13 @@ public class MockEmailService extends AbstractEmailService{
 		LOG.info("Simulando email");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
+	}
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
 	}
 
 }
