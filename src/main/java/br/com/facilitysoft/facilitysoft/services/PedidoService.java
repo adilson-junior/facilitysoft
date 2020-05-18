279fcf89d6bfb4c6daf1b0af5806a1fc780ea26c
adilson-junior <adilsonvajr@gmail.com>
2020-05-18 11:33:46 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/br/com/facilitysoft/facilitysoft/services/PedidoService.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/services/PedidoService.java
@@ -4,9 +4,14 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import br.com.facilitysoft.facilitysoft.dominio.Cliente;
 import br.com.facilitysoft.facilitysoft.dominio.ItemPedido;
 import br.com.facilitysoft.facilitysoft.dominio.PagamentoComBoleto;
 import br.com.facilitysoft.facilitysoft.dominio.Pedido;
@@ -14,6 +19,8 @@ import br.com.facilitysoft.facilitysoft.dominio.enums.EstadoPagamento;
 import br.com.facilitysoft.facilitysoft.repositories.ItemPedidoRepository;
 import br.com.facilitysoft.facilitysoft.repositories.PagamentoRepository;
 import br.com.facilitysoft.facilitysoft.repositories.PedidoRepository;
+import br.com.facilitysoft.facilitysoft.security.UserSS;
+import br.com.facilitysoft.facilitysoft.services.exceptions.AuthorizationException;
 import br.com.facilitysoft.facilitysoft.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,6 +75,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
