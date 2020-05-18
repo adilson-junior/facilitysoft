7eb81054813ddcc02391f142a3ae9329651aa013
adilson-junior <adilsonvajr@gmail.com>
2020-05-18 12:09:55 -0300

    Refresh token e esqueci a senha

--- src/main/java/br/com/facilitysoft/facilitysoft/resources/PedidoResources.java
+++ src/main/java/br/com/facilitysoft/facilitysoft/resources/PedidoResources.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.facilitysoft.facilitysoft.dominio.Categoria;
 import br.com.facilitysoft.facilitysoft.dominio.Pedido;
-import br.com.facilitysoft.facilitysoft.dto.CategoriaDTO;
 import br.com.facilitysoft.facilitysoft.services.PedidoService;
 
 @RestController
